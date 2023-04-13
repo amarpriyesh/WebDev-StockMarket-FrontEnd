b666528d6745968f6ef9d61525633b036da8893f
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-04-13 02:26:48 -0400

    google login changes

--- src/login/login.js
+++ src/login/login.js
@@ -6,88 +6,99 @@ import { useSelector } from "react-redux";
 import {GoogleLogin,GoogleLogout} from "react-google-login";
 
 function LoginScreen() {
-    const { currentUser } = useSelector((state) => state.user);
+    const {currentUser} = useSelector((state) => state.user);
     const [email, setEmail] = useState("");
-    const [user, setUser] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    var clientId="959350101705-iulfiifgd5jt2n09cuuu9vj3a9lnqb0v.apps.googleusercontent.com"
-    if (process.env.REACT_APP_ENVIRONMENT=="PRODUCTION") {
-        clientId="959350101705-2b65fq1nv6o2211ipkt87cb7a5askm82.apps.googleusercontent.com"
+    var clientId = "959350101705-iulfiifgd5jt2n09cuuu9vj3a9lnqb0v.apps.googleusercontent.com"
+    if (process.env.REACT_APP_ENVIRONMENT == "PRODUCTION") {
+        clientId = "959350101705-2b65fq1nv6o2211ipkt87cb7a5askm82.apps.googleusercontent.com"
     }
 
-
-
-    const responseGoogle = (response) => {
+    const responseGoogle = async (response) => {
         const user = {
             username: response.profileObj.givenName,
             password: response.profileObj.googleId,
-            email : response.profileObj.email,
+            email: response.profileObj.email,
             profilePhoto: response.profileObj.imageUrl,
             firstName: response.profileObj.givenName,
             lastName: response.profileObj.familyName
 
         }
-        console.log("google user",user)
-        dispatch(googleLoginThunk(user));
+        console.log("google user", user)
 
-        console.log('Logged In',currentUser);
+        const returnedUserResponse = await dispatch(googleLoginThunk(user));
+        if (returnedUserResponse.error) {
+            setError("Incorrect credentials");
+            navigate("/login")
+        } else {
+            navigate("/");
+        }
+
+        console.log('Logged In', currentUser);
 
     }
 
+
     const handleLogin = async () => {
-        dispatch(loginThunk({ email, password }));
-        navigate("/");
+        const response = await dispatch(loginThunk({email, password}));
+        if (response.error) {
+            setError("Incorrect credentials");
+            navigate("/login")
+        } else {
+            navigate("/");
+        }
     };
-    return (
-        <div className="container">
-            <h1>
-                <button onClick={handleLogin} className="float-end btn btn-primary">
-                    Login
-                </button>
+        return (
+            <div className="container">
+                <h1>
+                    <button onClick={handleLogin} className="float-end btn btn-primary">
+                        Login
+                    </button>
+                    <div>
+                        <GoogleLogin
+                            clientId={clientId}
+                            buttonText="Login with Google"
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            cookiePolicy={'single_host_origin'}
+                        /></div>
+                    Login Screen
+                </h1>
+                {error && (
+                    <div className="alert alert-danger" role="alert">
+                        {error}
+                    </div>
+                )}
                 <div>
-                    <GoogleLogin
-                        clientId={clientId}
-                        buttonText="Login with Google"
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
-                        cookiePolicy={'single_host_origin'}
-                    /></div>
-                Login Screen
-            </h1>
-            {error && (
-                <div className="alert alert-danger" role="alert">
-                    {error}
+                    <label>Email</label>
+                    <br/>
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
                 </div>
-            )}
-            <div>
-                <label>Email</label>
-                <br />
-                <input
-                    className="form-control"
-                    type="text"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-            </div>
-            <div>
-                <label>Password</label>
-                <input
-                    className="form-control"
-                    type="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-            </div>
-            {currentUser && (
                 <div>
-                    <h1>Welcome {currentUser.firstName} {currentUser.lastName}</h1>
+                    <label>Password</label>
+                    <input
+                        className="form-control"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
                 </div>
-            )}
-        </div>
-    );
-}
-export default LoginScreen;
+                {currentUser && (
+                    <div>
+                        <h1>Welcome {currentUser.firstName} {currentUser.lastName}</h1>
+                    </div>
+                )}
+            </div>
+        );
+    }
+
+export default LoginScreen
