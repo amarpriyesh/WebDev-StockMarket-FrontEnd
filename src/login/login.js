67bd53d6f03f1df8b07aed8101be0f044613dc7b
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-04-13 03:34:34 -0400

    gogle login changes

--- src/login/login.js
+++ src/login/login.js
@@ -1,15 +1,46 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
-import { loginThunk } from "../thunks/user-thunks";
+import { loginThunk,googleLoginThunk } from "../thunks/auth-thunks";
 import { useSelector } from "react-redux";
+import {GoogleLogin,GoogleLogout} from "react-google-login";
+
 function LoginScreen() {
-    const { currentUser } = useSelector((state) => state.user);
+    const {currentUser} = useSelector((state) => state.user);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
     const navigate = useNavigate();
     const dispatch = useDispatch();
+
+    var clientId = "959350101705-iulfiifgd5jt2n09cuuu9vj3a9lnqb0v.apps.googleusercontent.com"
+    if (process.env.REACT_APP_ENVIRONMENT == "PRODUCTION") {
+        clientId = "959350101705-2b65fq1nv6o2211ipkt87cb7a5askm82.apps.googleusercontent.com"
+    }
+
+    const responseGoogle = async (response) => {
+        const user = {
+            username: response.profileObj.givenName,
+            password: response.profileObj.googleId,
+            email: response.profileObj.email,
+            profilePhoto: response.profileObj.imageUrl,
+            firstName: response.profileObj.givenName,
+            lastName: response.profileObj.familyName
+
+        }
+        console.log("google user", user)
+
+        const returnedUserResponse = await dispatch(googleLoginThunk(user));
+        if (returnedUserResponse.error) {
+            setError("Incorrect credentials");
+            navigate("/login")
+        } else {
+            navigate("/");
+        }
+
+        console.log('Logged In', currentUser);
+
+    }
 
     const handleLogin =  async () => {
         const response = await dispatch(loginThunk({ email, password }));
@@ -26,6 +57,14 @@ function LoginScreen() {
                 <button onClick={handleLogin} className="float-end btn btn-primary">
                     Login
                 </button>
+                <div>
+                    <GoogleLogin
+                        clientId={clientId}
+                        buttonText="Login with Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    /></div>
                 Login Screen
             </h1>
             {error && (
