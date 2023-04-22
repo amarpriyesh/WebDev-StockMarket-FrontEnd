22bf7113cf1b3256dde5ae11f90573a3af42d0f5
parasswarup <parasswarup@gmail.com>
2023-04-22 01:58:45 -0400

    Updated search

--- src/search/searchResults.js
+++ src/search/searchResults.js
@@ -1,86 +1,101 @@
-import React, {useState} from 'react';
-import {Link, useLocation, useParams} from "react-router-dom";
-import axios from "axios";
-import {setSidebar} from "../reducers/sidebar-reducer";
-import {useDispatch} from "react-redux";
-import NewsItem from "../news/news-item";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setSidebar } from '../reducers/sidebar-reducer';
+import NewsItem from '../news/news-item';
 
 function SearchResults() {
-    const dispatch = useDispatch()
-    dispatch(setSidebar({component:"none",newsid:"ddd"}));
-    const { searchCriteria } = useParams();
-    const location = useLocation();
-    const [searchQuery, setSearchQuery] = useState(searchCriteria || '');
+    const dispatch = useDispatch();
+    dispatch(setSidebar({ component: 'none', newsid: 'ddd' }));
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchType, setSearchType] = useState('symbols');
     const [searchResults, setSearchResults] = useState([]);
 
     const handleSearch = (event) => {
         event.preventDefault();
-        const queryParams = new URLSearchParams(location.search);
-        queryParams.set('q', searchQuery);
-        window.location.search = queryParams.toString();
+        getSearchResults();
     };
 
     const handleInputChange = (event) => {
         setSearchQuery(event.target.value);
     };
 
+    const handleSearchTypeChange = (event) => {
+        setSearchType(event.target.value);
+    };
+
     const getSearchResults = () => {
-        const queryParams = new URLSearchParams(location.search);
-        const searchQuery = queryParams.get('q');
         if (searchQuery) {
-            axios.get(`https://api.marketaux.com/v1/news/all?symbols=${searchQuery}&filter_
-            entities=true&language=en&api_token=IKdLsrWdrAo18pM4p5DaEGSDDxgsugTVMnf5UDvs`)
-                .then(response => {
+            console.log(searchQuery);
+            console.log(searchType);
+            let apiUrl = `https://api.marketaux.com/v1/news/all?${searchType}=${searchQuery}`;
+            apiUrl += '&filter_entities=true&language=en&api_token=x9wRB4wjS2tQqDGHtVIxCQePoJzZjffPlbQZNj44';
+
+            axios
+                .get(apiUrl)
+                .then((response) => {
                     setSearchResults(response.data.data);
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.log(error);
                 });
         }
     };
 
-    // Call the API on first render if there's a search query in the URL
-    React.useEffect( () => {
-         getSearchResults();
-    }, []);
-
-    // Call the API whenever the search query changes
-    React.useEffect( () => {
-         getSearchResults();
-    }, [location.search]);
-
     return (
         <div className="container">
             <h2>Search Results</h2>
-            <form onSubmit={handleSearch}>
+            <form>
                 <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <label className="input-group-text">Search for:</label>
+                    </div>
+                    <select className="form-control" value={searchType} onChange={handleSearchTypeChange}>
+                        <option value="symbols">Symbols</option>
+                        <option value="exchanges">Exchanges</option>
+                        <option value="entity_types">Entity Types</option>
+                        <option value="industries">Industries</option>
+                        <option value="countries">Countries</option>
+                    </select>
                     <input
                         type="text"
                         className="form-control"
-                        placeholder="Search"
+                        placeholder={`Search ${searchType}`}
                         value={searchQuery}
                         onChange={handleInputChange}
                     />
-                    <button className="btn btn-primary" type="submit">Search</button>
+                    <button className="btn btn-primary" onClick={handleSearch}>
+                        Search
+                    </button>
                 </div>
             </form>
-            {searchResults ?
-                <div>
-                    <ul className="list-group">
-                        {searchResults.map(data => <NewsItem news={{_id:data.uuid,title:data.title,description:data.description,image:data.image_url,source:data.source,time:data.published_at,symbol:data.entities[0].symbol,company:data.entities[0].name,industry:data.entities[0].industry,sentiment:data.entities[0].sentiment_score}} key={data._id}/>)}
-                    </ul>
-                </div> :
+            {searchResults.length > 0 ? (
+                <ul className="list-group">
+                    {searchResults.map((data) => (
+                        <NewsItem
+                            news={{
+                                _id: data.uuid,
+                                title: data.title,
+                                description: data.description,
+                                image: data.image_url,
+                                source: data.source,
+                                time: data.published_at,
+                                symbol: data.entities[0].symbol,
+                                company: data.entities[0].name,
+                                industry: data.entities[0].industry,
+                                sentiment: data.entities[0].sentiment_score,
+                            }}
+                            key={data._id}
+                        />
+                    ))}
+                </ul>
+            ) : (
                 <div className="container">
-                    <p>No results found for "{searchQuery}"</p>
-                    <Link to="/details/123" className="btn btn-primary">
-                        Details
-                    </Link>
+                    {searchQuery && <p>No results found for "{searchQuery}"</p>}
                 </div>
-            }
+            )}
         </div>
     );
-
-
 }
 
 export default SearchResults;
