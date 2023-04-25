e07ff12dd8a16d59a15f02f252d404f0342413ba
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-04-25 07:13:48 -0400

    Search/pagination

--- src/services/news-service.js
+++ src/services/news-service.js
@@ -4,22 +4,33 @@ const API_URL = process.env.REACT_APP_API_URL
 const TOKEN = process.env.REACT_APP_API_TOKEN
 
 
-export const findAllNews = async () => {
-    const response = await axios.get(`${BASE_URL}/news`);
-    const news = response.data;
-    return news;
+export const findAllNews = async (page,pattern) => {
+    if(pattern==null) {
+        const response = await axios.put(`${BASE_URL}/news?page=${page}`);
+        const news = response.data;
+        return news;
+    }
+    else{
+        const response = await axios.put(`${BASE_URL}/news?page=${page}&&pattern=${pattern}`);
+        const news = response.data;
+        return news;
+    }
+
 };
 
 export const findNewsByID = async (id1) => {
 
     const req = API_URL+id1+"?api_token="+TOKEN
-    //const req = BASE_URL+"/news/"+id1
-    console.log("ID in service",req)
     const response = await axios.get(`${req}`);
     const news = response.data;
     return news.entities;
 };
 
+export const findAllNewsPattern = async (pattern) => {
+    const response = await axios.get(`${BASE_URL}/news/pattern/${pattern}`);
+    const news = response.data;
+    return news;
+};
 
 
 
